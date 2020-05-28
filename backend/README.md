44c1683a93efefd9d0d3f603532eecdc12396731
ericoandre <ericoandresilva@gmail.com>
2020-05-28 14:08:54 -0300

    r

--- backend/README.md
+++
@@ -1,5 +0,0 @@
-# Arquivo de configuração do banco de dados
-
-.env.exemplo renomear para .env
-nesse arquivo vc define a porta, ip e nome da tabela do seu banco,
-e apalavra secreta do JWT
--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const server = require('./server')
-
-const porta = process.env.PORT || 8080
-
-server.listen(porta, () => {
-	console.log(`Servidor de pé em http://localhost:${porta}`)
-	console.log('Pra derrubar o servidor: ctrl + c')
-})
